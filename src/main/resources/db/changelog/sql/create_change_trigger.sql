dc79114e4700626daee62c7f6aa119d1733d7da6
grzybek <grzybol.k@gmail.com>
2022-03-14 21:56:52 +0100

    saving changes on tables

---
+++ src/main/resources/db/changelog/sql/create_change_trigger.sql
@@ -0,0 +1,31 @@
+CREATE FUNCTION change_trigger() RETURNS trigger AS
+$$
+BEGIN
+
+    IF TG_OP = 'INSERT'
+    THEN
+
+        INSERT INTO change
+        VALUES (now());
+
+        RETURN NEW;
+    ELSIF TG_OP = 'UPDATE'
+    THEN
+
+        INSERT INTO change
+        VALUES (now());
+
+        RETURN NEW;
+    ELSIF TG_OP = 'DELETE'
+    THEN
+
+        INSERT INTO change
+        VALUES (now());
+
+        RETURN OLD;
+    END IF;
+
+    RETURN NEW;
+END;
+
+$$ LANGUAGE 'plpgsql';
