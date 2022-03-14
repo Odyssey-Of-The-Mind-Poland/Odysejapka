62d8c82e737dd09b888f28aa203044add5496038
grzybek <grzybol.k@gmail.com>
2022-03-14 19:21:51 +0100

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 gradle build
 docker build -t grzybek/odysejapka .
-ssh grzybek docker stop $(docker ps -q --filter ancestor=grzybek/odysejapka )
+ssh grzybek docker kill $(ssh grzybek docker ps -q --filter ancestor=grzybek/odysejapka )
 docker save grzybek/odysejapka | bzip2 | pv | ssh grzybek docker load
 ssh grzybek docker run -p 8081:8081 --net=host -d grzybek/odysejapka
