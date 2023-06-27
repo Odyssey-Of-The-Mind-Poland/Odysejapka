a7b725df854e9bf1967c058307d3d6fb94bd7cf8
kjubik <wojt.kubicki@gmail.com>
2023-06-27 15:19:51 +0200

    added alt text to logo

--- odyseja-ui/src/routes/+page.svelte
+++ odyseja-ui/src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-10 text-center">
     <div class="card p-4">
-      <section><img src="logo.png"></section>
+      <section><img src="logo.png" alt="logo Odyseja Umysłu"></section>
       <footer class="p-4 card-footer">
         <button type="button" class="btn btn-xl variant-filled-primary" on:click={handleLogin}>
           Zaloguj
