653f48a53aa54d2bc073fae11736b10ddfeb031f
kjubik <wojt.kubicki@gmail.com>
2023-06-27 20:56:56 +0200

    removed header, added logo

--- odyseja-ui/src/routes/panel/+layout.svelte
+++ odyseja-ui/src/routes/panel/+layout.svelte
@@ -3,6 +3,7 @@
   import {logout} from "../../authService";
   import {AppBar, AppShell, LightSwitch, Toast} from "@skeletonlabs/skeleton";
   import Navigation from "$lib/Navigation/Navigation.svelte";
+  import logo from '$lib/assets/logo.png'
 
   const menuItems = [
     { label: 'Harmonogram', route: '/panel/timetable', icon: 'ic:round-calendar-view-month' },
@@ -13,24 +14,23 @@
 <Toast />
 
 <AppShell slotSidebarLeft="bg-surface-500/5 w-64 p-4">
-  <svelte:fragment slot="header">
-    <AppBar>
-      <svelte:fragment slot="lead">
-        <strong class="text-2xl">Odyseja Umysłu</strong>
-      </svelte:fragment>
-        <LightSwitch class="float-right"/>
-    </AppBar>
-  </svelte:fragment>
 
   <div class="p-6">
     <slot />
   </div>
   <svelte:fragment slot="sidebarLeft">
     <div class="flex h-full flex-col">
-        <Navigation menuItems={menuItems} />
+      <div class="mt-4 mb-12 text-center">
+        <a href="/panel">
+          <img src="{logo}" alt="Logo Odysei Umysłu" class="h-2/5 m-auto"/>
+          <span class="text-2xl font-semibold text-primary-600">Odyseja Umysłu</span>
+        </a>
+      </div>
+      <Navigation menuItems={menuItems} />
       <button type="button" class="btn btn-sm variant-filled-primary" on:click={logout}>
         Logout
       </button>
+      <LightSwitch class=""/>
     </div>
   </svelte:fragment>
 </AppShell>
