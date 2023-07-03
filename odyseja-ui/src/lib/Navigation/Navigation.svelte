b502325bf898cb1be3946bfbef2edf8d2db45417
kjubik <wojt.kubicki@gmail.com>
2023-07-03 02:34:11 +0200

    decrease margin top

--- odyseja-ui/src/lib/Navigation/Navigation.svelte
+++ odyseja-ui/src/lib/Navigation/Navigation.svelte
@@ -7,7 +7,7 @@
 <nav class="flex-auto">
     <ul class="mx-6">
         {#each menuItems as item}
-            <li class="rounded-md mt-6 first:mt-0 hover:outline hover:outline-1 hover:outline-dark-500">
+            <li class="rounded-md mt-4 first:mt-0 hover:outline hover:outline-1 hover:outline-dark-500">
                 <a href="{item.route}" class="text-left px-4 py-4 flex items-center text-dark-500">
                     <Icon icon="{item.icon}" class="text-3xl inline-flex"/>
                     <span class="text-lg font-normal pl-2">{item.label}</span>
