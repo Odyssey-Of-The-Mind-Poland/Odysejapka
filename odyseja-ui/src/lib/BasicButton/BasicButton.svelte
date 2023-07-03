0db3b67a778d2f2aa41fc98bfd7cf39b21b775ac
kjubik <wojt.kubicki@gmail.com>
2023-07-03 02:33:50 +0200

    add button component

---
+++ odyseja-ui/src/lib/BasicButton/BasicButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+    export let text, icon;
+
+</script>
+
+<button class="hover:bg-white outline outline-1 outline-dark-500 hover:outline-dark-200 px-3 py-1 rounded
+    flex justify-center items-center text-dark-500 hover:text-dark-300">
+        <span class="pr-2 text-lg">{text}</span>
+        <span class="material-symbols-rounded text-md">{icon}</span>
+</button>
