d001f8d1b173a73e470bb14a69fcf9720deb659b
1grzyb1 <grzybol.k@gmail.com>
2023-07-07 23:20:30 +0200

    Revert "toggle editing + conditional styling"
    This reverts commit 82e5ca11

--- odyseja-ui/src/routes/panel/problem/+page.svelte
+++ odyseja-ui/src/routes/panel/problem/+page.svelte
@@ -1,27 +1,7 @@
 <script lang="ts">
   import { saveProblems } from '../apiService';
   import type { Problems } from '../types';
-  import BasicButton from "$lib/Buttons/BasicButton.svelte";
-
-  let isEditable = false, showModal = false;
-
-  function addProblem() {
-    showModal = !showModal;
-  }
-
-  function toggleEdit() {
-    isEditable = !isEditable;
-  }
-
-  function saveClicked() {
-    toggleEdit();
-    save();
-  }
-
-  function cancelClicked() {
-    toggleEdit();
-    data = JSON.parse(JSON.stringify(initialData));
-  }
+  import BasicButton from "$lib/BasicButton/BasicButton.svelte";
 
   export let data: Problems;
 
@@ -33,14 +13,13 @@
     saveProblems(data);
     initialData = JSON.parse(JSON.stringify(data));
   }
-
 </script>
 
   <header class="h-40 bg-neutral-100 flex items-center justify-between px-12 w-full">
     <h2 class="text-4xl text-dark-500 font-medium">Problemy</h2>
     <div class="flex gap-4">
-<!--      <BasicButton text="Dodaj" icon="add" on:click={addProblem}/>-->
-      <BasicButton text="Edytuj" icon="edit" on:click={toggleEdit} disabled="{isEditable}"/>
+      <BasicButton text="Dodaj" icon="add"/>
+      <BasicButton text="Edytuj" icon="edit" />
     </div>
   </header>
 
@@ -48,27 +27,15 @@
     {#each data.problems as problem}
       <div class="first:mt-0 mt-4">
         <span class="text-sm font-normal text-dark-200 ml-2">Numer {problem.id}</span>
-          <div class="w-full h-full flex items-center rounded outline outline-1 outline-neutral-200 focus:outline-blue-500">
-            {#if isEditable}
-              <span class="material-symbols-rounded text-dark-200 px-2">drag_indicator</span>
-              <input type="text" bind:value={problem.name} class="border-0 focus:ring-0 h-full w-full
-              px-0 py-2 text-xl text-dark-300 focus:text-dark-500">
-            {:else}
-              <input disabled type="text" bind:value={problem.name} class="border-0 h-full w-full text-xl text-dark-500">
-            {/if}
-          </div>
+        <div class="rounded outline outline-1 outline-neutral-200 px-4 py-2">
+          <span class="text-xl text-dark-500">{problem.name}</span>
+        </div>
       </div>
     {/each}
   </section>
 
-  {#if isEditable}
-    <footer class="w-full py-4 px-12 flex justify-center gap-4">
-      <BasicButton text="Zapisz" icon="check" on:click={saveClicked} disabled='{!isChanged}'/>
-      <BasicButton text="Anuluj" icon="close" on:click={cancelClicked}/>
-    </footer>
-    {#if isChanged}
-      <span class="text-blue-500">Są zmiany do zapisania</span>
-    {:else}
-      <span class="text-dark-100">Brak zmian</span>
-    {/if}
-  {/if}
+<!--  <footer class="">-->
+<!--    <button type="button" on:click={save} disabled='{!isChanged}' class="btn btn-md variant-filled-primary">-->
+<!--      Zapisz-->
+<!--    </button>-->
+<!--  </footer>-->
