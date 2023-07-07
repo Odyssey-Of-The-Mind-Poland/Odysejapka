0b5e984d65e9fbd0d72e6ee397fba5ec87710921
1grzyb1 <grzybol.k@gmail.com>
2023-07-07 23:18:20 +0200

    Revert "enhanced styling and new menu items in Sidebar"
    This reverts commit f5ef3309

--- odyseja-ui/src/routes/panel/+layout.svelte
+++ odyseja-ui/src/routes/panel/+layout.svelte
@@ -1,8 +1,9 @@
 <!-- src/components/Sidebar.svelte -->
 <script>
   import {logout} from "../../authService";
+  import {AppBar, AppShell, LightSwitch, Toast} from "@skeletonlabs/skeleton";
   import Navigation from "$lib/Navigation/Navigation.svelte";
-  import Icon from '@iconify/svelte'
+  import logo from '$lib/assets/logo.png'
 
   const menuItems = [
     { label: 'Harmonogram', route: '/panel/timetable', icon: 'ic:round-calendar-view-month' },
@@ -10,35 +11,26 @@
   ];
 </script>
 
-<div class="grid grid-cols-12">
+<Toast />
 
-  <div class="col-span-2 border-r border-r-neutral-200">
-    <div class="flex h-full flex-col">
+<AppShell slotSidebarLeft="bg-surface-500/5 w-64 p-4">
 
-      <div class="mt-12 mb-16 text-center flex flex-col items-center">
-        <h1 class="text-2xl font-semibold text-dark-500">Odyseja Umysłu</h1>
-        <div class="bg-orange-50 px-4 rounded-full outline outline-1 outline-orange-500 mt-2">
-          <h2 class="text-orange-500 text-md">Admin</h2>
-        </div>
-      </div>
-
-      <Navigation menuItems={menuItems} />
-
-      <hr class="h-px mx-6">
-      <button on:click={logout} class="group/button my-6 mx-6 py-4 rounded-lg hover:bg-orange-400/20 hover:outline outline-1 outline-orange-500">
-        <span class="flex ml-2 items-center">
-          <span class="bg-orange-500 h-9 w-9 rounded-full mr-3 flex justify-center items-center">
-            <Icon icon="ic:round-log-out" class="text-white text-2xl inline-flex"/>
-          </span>
-          <span class="text-md font-medium text-dark-500 group-hover/button:text-orange-600">Wyloguj</span>
-        </span>
-      </button>
-
-    </div>
-  </div>
-
-  <div class="col-span-10 min-h-screen p-6">
+  <div class="p-6">
     <slot />
   </div>
-
-</div>
+  <svelte:fragment slot="sidebarLeft">
+    <div class="flex h-full flex-col">
+      <div class="mt-4 mb-12 text-center">
+        <a href="/panel">
+          <img src="{logo}" alt="Logo Odysei Umysłu" class="h-2/5 m-auto"/>
+          <span class="text-2xl font-semibold text-primary-600">Odyseja Umysłu</span>
+        </a>
+      </div>
+      <Navigation menuItems={menuItems} />
+      <button type="button" class="btn btn-sm variant-filled-primary" on:click={logout}>
+        Logout
+      </button>
+      <LightSwitch class=""/>
+    </div>
+  </svelte:fragment>
+</AppShell>
--- odyseja-ui/src/routes/panel/+page.svelte
+++ odyseja-ui/src/routes/panel/+page.svelte
@@ -1,6 +1,1 @@
-<script>
-import {LightSwitch} from "@skeletonlabs/skeleton";
-</script>
-
-<span class="dark:text-blue-950">widać mnie, nie widać mnie</span>
-<LightSwitch></LightSwitch>
+panel
