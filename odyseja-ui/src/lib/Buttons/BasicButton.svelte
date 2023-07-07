7a88954a4e7c44e265a9e5aa8a662f816c1e67df
1grzyb1 <grzybol.k@gmail.com>
2023-07-07 23:20:30 +0200

    Revert "fix click event on button component"
    This reverts commit 8d132f22049117ab4b853e74342ee0ccefd71b6e.

--- odyseja-ui/src/lib/Buttons/BasicButton.svelte
+++ odyseja-ui/src/lib/Buttons/BasicButton.svelte
@@ -4,7 +4,7 @@
 
 </script>
 
-<button on:click class="hover:bg-white outline outline-1 outline-dark-500 hover:outline-dark-200 px-3 py-1 rounded
+<button class="hover:bg-white outline outline-1 outline-dark-500 hover:outline-dark-200 px-3 py-1 rounded
     flex justify-center items-center text-dark-500 hover:text-dark-300">
         <span class="pr-2 text-lg">{text}</span>
         <span class="material-symbols-rounded text-md">{icon}</span>
