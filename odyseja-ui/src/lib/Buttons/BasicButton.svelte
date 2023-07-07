39e954261adef6ed55ecc03b4266891ab77c8144
1grzyb1 <grzybol.k@gmail.com>
2023-07-07 23:20:26 +0200

    Revert "button can be disabled from parent"
    This reverts commit fbe9b37a

--- odyseja-ui/src/lib/Buttons/BasicButton.svelte
+++ odyseja-ui/src/lib/Buttons/BasicButton.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+
     export let text, icon;
-    export let disabled = false;
+
 </script>
 
-<button on:click {disabled} class="hover:bg-white outline outline-1 outline-dark-500 hover:outline-dark-200 px-3 py-1 rounded
+<button on:click class="hover:bg-white outline outline-1 outline-dark-500 hover:outline-dark-200 px-3 py-1 rounded
     flex justify-center items-center text-dark-500 hover:text-dark-300">
         <span class="pr-2 text-lg">{text}</span>
         <span class="material-symbols-rounded text-md">{icon}</span>
