2d61a087185f3468db393dd0f913591004d4a652
kjubik <wojt.kubicki@gmail.com>
2023-08-10 20:52:15 +0200

    add toggled state for editing stages

--- odyseja-ui/src/routes/panel/stage/+page.svelte
+++ odyseja-ui/src/routes/panel/stage/+page.svelte
@@ -13,11 +13,27 @@ export let data: Stages;
         initialData = JSON.parse(JSON.stringify(data))
     }
 
+    let editToggled = false
+    function toggleEdit() {
+        editToggled = !editToggled;
+        editToggled = editToggled;
+    }
+
+    function cancelChanges() {
+        toggleEdit();
+        data = JSON.parse(JSON.stringify(initialData));
+    }
+
 </script>
 
-    <section class="p-4">
+    <div class="mt-4 mx-4 flex justify-between items-center">
+        <h2 class="mb-6">Problemy</h2>
+        <button type="button" on:click={toggleEdit} class="btn btn-md variant-filled-primary"
+                disabled='{editToggled}'>Edytuj</button>
+    </div>
+    <section class="p-4 flex flex-wrap">
         {#each data.stages as stage}
-            <label class="label m-4">
+            <label class="label p-2 w-1/3">
                 <span>Scena nr. {stage.id}</span>
                 <input class="input" type="text" placeholder="Input" bind:value={stage.name}/>
             </label>
@@ -25,7 +41,10 @@ export let data: Stages;
     </section>
 
     <footer class="pl-8">
-        <button type="button" on:click={save} disabled='{!isChanged}' class="btn btn-md variant-filled-primary">
+        <button type="button" on:click={save} disabled='{!isChanged}' class="btn btn-md variant-filled-secondary">
             Zapisz
+        </button>
+        <button type="button" on:click={cancelChanges} disabled='{!editToggled}' class="btn btn-md variant-ringed-surface">
+            Anuluj
         </button>
     </footer>
