844fe55901341d9ecf50d32dad7dbfb0157821c8
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-01-25 13:39:18 +0100

    fix: show one upload image button on sponsor page

--- odyseja-ui/src/routes/panel/sponsor/+page.svelte
+++ odyseja-ui/src/routes/panel/sponsor/+page.svelte
@@ -12,6 +12,7 @@
         data = await fetchSponsors();
     });
 
+    let rowIndex = 0;
 
     async function handleImageUpload(event: Event, index: number) {
         const file = (event.target as HTMLInputElement).files[0];
@@ -23,10 +24,11 @@
                 }
             };
             reader.readAsDataURL(file);
+            rowIndex++;
         }
     }
 
-    async function uploadImageToServer(file, row, column) {
+    async function uploadImageToServer(file: File, row: number, column: number) {
         const formData = new FormData();
         formData.append('image', file);
         formData.append('row', row.toString());
@@ -54,6 +56,11 @@
 </script>
 
 <h2 class="mb-6">Sponsorzy</h2>
+<label class="bg-green-500 text-white px-4 py-2 rounded cursor-pointer w-32 text-center">
+    Add Image
+    <input type="file" class="hidden" accept="image/*"
+            on:change={(e) => handleImageUpload(e, rowIndex)}/>
+</label>
 
 <main class="p-4">
     <table class="w-full mt-4">
@@ -65,17 +72,10 @@
                         <button
                                 type="button"
                                 class="btn btn-md variant-filled-error ml-4"
-                                on:click={deletePerformance(sponsor.id)}>Usuń
+                                on:click={() => deletePerformance(sponsor.id)}>Usuń
                         </button>
                     </td>
                 {/each}
-                <td class="border p-2">
-                    <label class="bg-green-500 text-white px-4 py-2 rounded cursor-pointer">
-                        Add Image
-                        <input type="file" class="hidden" accept="image/*"
-                               on:change={(e) => handleImageUpload(e, rowIndex)}/>
-                    </label>
-                </td>
             </tr>
         {/each}
     </table>
