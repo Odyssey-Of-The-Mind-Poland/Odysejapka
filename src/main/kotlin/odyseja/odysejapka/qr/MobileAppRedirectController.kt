02be9020b55be708e3d2d574a90428d95545f5c0
1grzyb1 <grzybol.k@gmail.com>
2024-03-23 13:47:48 +0100

    feat: getting device type

---
+++ src/main/kotlin/odyseja/odysejapka/qr/MobileAppRedirectController.kt
@@ -0,0 +1,34 @@
+package odyseja.odysejapka.qr
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("/lappka/download")
+internal class MobileAppRedirectController {
+    @GetMapping
+    fun detectDevice(request: HttpServletRequest): String {
+        val userAgent = request.getHeader("User-Agent")
+        var deviceType = getDeviceType(userAgent)
+
+        return "Detected Device Type: $deviceType"
+    }
+
+    private fun getDeviceType(userAgent: String): String {
+
+        if (userAgent.lowercase(Locale.getDefault()).contains("android")) {
+            return "Android"
+        }
+
+        if (userAgent.lowercase(Locale.getDefault())
+                .contains("iphone") || userAgent.lowercase(Locale.getDefault()).contains("ipad")
+        ) {
+            return "iOS"
+        }
+
+        return "Other"
+    }
+}
