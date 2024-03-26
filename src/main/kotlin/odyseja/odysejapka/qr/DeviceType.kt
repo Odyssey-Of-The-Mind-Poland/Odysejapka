c3641107ed60f74b3f9a820147d03534c73bec08
1grzyb1 <grzybol.k@gmail.com>
2024-03-26 19:58:03 +0100

    feat: set proper app urls

--- src/main/kotlin/odyseja/odysejapka/qr/DeviceType.kt
+++ src/main/kotlin/odyseja/odysejapka/qr/DeviceType.kt
@@ -1,7 +1,7 @@
 package odyseja.odysejapka.qr
 
 enum class DeviceType(val location: String) {
-    ANDROID("https://play.google.com/store/games?hl=pl&gl=US"),
-    IOS("https://www.apple.com/pl/app-store/"),
-    OTHER("/static/download.html")
+    ANDROID("https://play.google.com/store/apps/details?id=pl.odyssey.lappka"),
+    IOS("https://apps.apple.com/pl/app/%C5%82appka-omera/id1616380523"),
+    OTHER("https://app.odyseja.org/")
 }
