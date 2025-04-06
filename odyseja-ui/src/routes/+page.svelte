2c119210254170b4e722a364aec053252786f45c
vicctor <artur.keska@gmail.com>
2025-04-06 15:14:28 +0200

    Revert "feat: logging out after expiration"
    This reverts commit 6830a747674bd20c5e7959f3581d63a8747c9cd9.

--- odyseja-ui/src/routes/+page.svelte
+++ odyseja-ui/src/routes/+page.svelte
@@ -1,40 +1,10 @@
-<script lang="ts">
-  import {login, logout} from '../authService'
-  import Token from '../token';
-  import { onMount, onDestroy } from 'svelte';
-  import { goto } from '$app/navigation';
-
-  let isExpired = false;
-  let checkInterval: number;
-
-  function checkToken() {
-    const token = new Token(document.cookie.split('; ').find(row => row.startsWith('access_token='))?.split('=')[1]);
-    if (token) {
-      isExpired = token.isExpired();
-      
-      if (isExpired) {
-        logout();
-        goto('/');
-      }
-    }
-  }
-
-  onMount(() => {
-    checkToken();
-    checkInterval = window.setInterval(checkToken, 10000);
-  });
-
-  onDestroy(() => {
-    if (checkInterval) {
-      clearInterval(checkInterval);
-    }
-  });
+<script>
+  import {login} from '../authService'
 
   function handleLogin() {
     login();
   }
 </script>
-
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-10 text-center">
     <div class="card p-4">
