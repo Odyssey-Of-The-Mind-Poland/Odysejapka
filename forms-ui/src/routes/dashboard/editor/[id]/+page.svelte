c95158032b2ea1ac9f1f13fbc4d956ffd7548732
grzybek <grzybol.k@gmail.com>
2025-11-22 16:13:11 +0100

    feat: fetch problem form

---
+++ forms-ui/src/routes/dashboard/editor/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import {apiFetch} from "$lib/api";
+    import {Spinner} from "$lib/components/ui/spinner";
+    import {page} from "$app/state";
+
+    let formPromise = $derived(apiFetch(`/api/v1/form/${page.params.id}`));
+</script>
+
+{#await formPromise}
+    <Spinner/>
+{:then form}
+    {JSON.stringify(form)}
+{:catch error}
+    <p>Something went wrong: {error.message}</p>
+{/await}
