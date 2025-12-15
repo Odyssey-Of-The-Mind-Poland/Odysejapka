a5011c2fd05257e5470c045091d2cd5aec5b322e
grzybek <grzybol.k@gmail.com>
2025-12-15 18:48:20 +0100

    fix: add spinner

---
+++ forms-ui/src/lib/components/ui/spinner/spinner.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {cn} from "$lib/utils.js";
+    import Loader2Icon from "@lucide/svelte/icons/loader-2";
+    import type {ComponentProps} from "svelte";
+
+    let {class: className, ...restProps}: ComponentProps<typeof Loader2Icon> = $props();
+</script>
+
+<Loader2Icon
+        {...restProps}
+        aria-label="Loading"
+        class={cn("size-4 animate-spin", className)}
+        role="status"
+/>
