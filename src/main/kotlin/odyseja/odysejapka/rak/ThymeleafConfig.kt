2656b8a96a6713737229ea1a5ba3503800560f46
grzybek <grzybol.k@gmail.com>
2026-01-04 13:38:58 +0100

    feat: template resolver

--- src/main/kotlin/odyseja/odysejapka/rak/ThymeleafConfig.kt
+++ src/main/kotlin/odyseja/odysejapka/rak/ThymeleafConfig.kt
@@ -1,12 +1,9 @@
 package odyseja.odysejapka.rak
 
-import org.springframework.context.ApplicationContext
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import org.springframework.beans.factory.annotation.Value
 import org.thymeleaf.spring5.SpringTemplateEngine
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
 import org.thymeleaf.templatemode.TemplateMode
 import org.thymeleaf.templateresolver.FileTemplateResolver
 
@@ -16,21 +13,21 @@ class ThymeleafConfig {
     @Value("\${spring.thymeleaf.prefix}")
     private lateinit var templatePrefix: String
 
-    @Bean
-    fun templateResolver(): FileTemplateResolver {
-        val templateResolver = FileTemplateResolver()
-        templateResolver.prefix = templatePrefix
-        templateResolver.suffix = ".html"
-        templateResolver.templateMode = TemplateMode.HTML
-        templateResolver.characterEncoding = "UTF-8"
-        templateResolver.isCacheable = false
-        return templateResolver
-    }
+//    @Bean
+//    fun templateResolver(): FileTemplateResolver {
+//        val templateResolver = FileTemplateResolver()
+//        templateResolver.prefix = templatePrefix
+//        templateResolver.suffix = ".html"
+//        templateResolver.templateMode = TemplateMode.HTML
+//        templateResolver.characterEncoding = "UTF-8"
+//        templateResolver.isCacheable = false
+//        return templateResolver
+//    }
 
     @Bean
-    fun templateEngine(): SpringTemplateEngine {
+    fun templateEngine(databaseTemplateResolver: DatabaseTemplateResolver): SpringTemplateEngine {
         val templateEngine = SpringTemplateEngine()
-        templateEngine.setTemplateResolver(templateResolver())
+        templateEngine.setTemplateResolver(databaseTemplateResolver)
         templateEngine.enableSpringELCompiler = true
         return templateEngine
     }
