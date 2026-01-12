4750f6c8638edbf32510697394180398f1f53a31
grzybek <grzybol.k@gmail.com>
2026-01-12 14:17:10 +0100

    ci: run gradle test

--- src/test/kotlin/odyseja/odysejapka/OdysejapkaApplicationTests.kt
+++ src/test/kotlin/odyseja/odysejapka/OdysejapkaApplicationTests.kt
@@ -1,9 +1,17 @@
 package odyseja.odysejapka
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@Import(TestcontainersConfiguration::class)
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = ["odyseja.odysejapka"])
+@ActiveProfiles("test")
 class OdysejapkaApplicationTests {
 
 	@Test
