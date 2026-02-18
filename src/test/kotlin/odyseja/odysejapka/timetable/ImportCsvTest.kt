5dd4db541afcd6141f824557db6e41305aa6a70b
przemek54 <przemek_kvapil@onet.pl>
2026-02-18 18:19:45 +0100

    fix: validation tests

--- src/test/kotlin/odyseja/odysejapka/timetable/ImportCsvTest.kt
+++ src/test/kotlin/odyseja/odysejapka/timetable/ImportCsvTest.kt
@@ -25,7 +25,6 @@ class ImportCsvTest: OdysejaDsl() {
 
         Assertions.assertThat(timeTableClient.getPerformances(city.id)).hasSize(280)
 
-        // val mockPerformance = PerformanceEntity()
         val realPerformance = timeTableClient.getPerformance(1)
         val mockPerformance = Performance(
             id = 1,
@@ -47,6 +46,8 @@ class ImportCsvTest: OdysejaDsl() {
 
         Assertions.assertThat(realPerformance).isEqualTo(mockPerformance)
     }
+
+    @Test
     fun `should reject invalid inputs`() {
         val city = createCity("finał")
 
@@ -58,33 +59,40 @@ class ImportCsvTest: OdysejaDsl() {
         val badSpontan = mockCsv(spontan = "00:000")
         val badName = mockCsv(name = "")
 
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badPerformanceDay, city.id)
         }
-            .withMessageContaining("Dozwolone dni występu to sobota lub niedziela.")
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Dozwolone dni występu to sobota lub niedziela.")
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badSpontanDay, city.id)
         }
-            .withMessageContaining("Dozwolone dni spontana to sobota lub niedziela.")
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Dozwolone dni spontana to sobota lub niedziela.")
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badProblem, city.id)
         }
-            .withMessageContaining("Numer problemu musi wynosić od 0 do 5.")
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Numer problemu musi wynosić od 0 do 5.")
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badAge, city.id)
         }
-            .withMessageContaining("Numer grupy wiekowej musi wynosić od 0 do 4.")
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Numer grupy wiekowej musi wynosić od 0 do 4.")
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badPerformance, city.id)
         }
-            .withMessageContaining("Godzina występu powinna być w następującym formacie: 08:45.")
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Godzina występu powinna być w następującym formacie: 08:45.")
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badSpontan, city.id)
         }
-            .withMessageContaining("Godzina spontana powinna być w następującym formacie: 08:45.")
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Godzina spontana powinna być w następującym formacie: 08:45.")
+        Assertions.assertThatThrownBy {
             timeTableClient.importPerformances(badName, city.id)
         }
-            .withMessageContaining("Nazwa drużyny nie może być pusta.")
+            .hasRootCauseInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Nazwa drużyny nie może być pusta.")
     }
 }
