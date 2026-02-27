c782e6174e588edaa073be169402e00a7d865a99
przemek54 <przemek_kvapil@onet.pl>
2026-02-27 22:00:09 +0100

    feat: clear stages

--- src/main/kotlin/odyseja/odysejapka/stage/StageController.kt
+++ src/main/kotlin/odyseja/odysejapka/stage/StageController.kt
@@ -11,12 +11,18 @@ class StageController(
 
   @GetMapping()
   fun getStages(@RequestParam(required = false) cityId: Int?): List<Stage> {
-    return cityId?.let { stageService.getStages(cityId) } ?: stageService.getStages(0)
+    return cityId?.let { stageService.getStagesByCity(cityId) } ?: stageService.getStagesByCity(0)
   }
 
   @Secured("ROLE_ADMINISTRATOR")
   @PutMapping
   fun updateStage(@RequestBody stages: List<Stage>) {
     return stageService.updateStage(stages)
+  }
+
+  @Secured("ROLE_ADMINISTRATOR")
+  @DeleteMapping("/{cityId}")
+  fun clearStagesByCity(@PathVariable cityId: Int) {
+    stageService.clearStagesByCity(cityId)
   }
 }
