b9f9147fecdb630e4e51b226000e40acb4708f89
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-03-03 16:12:45 +0100

    Add forms ui to deployment

--- forms-ui/vite.config.ts
+++ forms-ui/vite.config.ts
@@ -1,14 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
 
 export default defineConfig({
-	preprocess: vitePreprocess({ script: true }),
-	kit: {
-		adapter: adapter()
-	},
 	plugins: [tailwindcss(), sveltekit()],
 	preview: {
 		allowedHosts: ['grzybek.snet.ovh']
